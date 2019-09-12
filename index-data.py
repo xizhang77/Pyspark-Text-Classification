a7e2c3d8483b52b81eb18daacdeaae3578f4c742
Xi Zhang <xizhang1@cs.stonybrook.edu>
2019-09-11 17:43:37 -0700

    Update index-data.py

--- index-data.py
+++ index-data.py
@@ -11,8 +11,8 @@ import sys,os,re,glob,nltk
 STATIC_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data/')
 os.chdir(STATIC_DIR)
 
-stopwords = nltk.corpus.stopwords.words('english')
-
+stemmer = nltk.stem.snowball.SnowballStemmer("english")
+# stopwords = nltk.corpus.stopwords.words('english')
 
 doc_context = [] 
 doc_label = []
@@ -21,14 +21,16 @@ for file in glob.glob("*/*"):
 	with open( file, 'r' ) as f:
 		context = []
 		for line in f.readlines():	
-			match = re.findall(r'[a-zA-Z]+', line) #[\w]: matches any alphanumeric character and the underscore; equivalent to [a-zA-Z0-9_].
-			context +=  match 
-		# print lines_list
-		texts = [word for word in context if word not in stopwords]
-		out_str=' '.join(texts)
+			texts = re.findall(r'[a-zA-Z]+', line) #[\w]: matches any alphanumeric character and the underscore; equivalent to [a-zA-Z0-9_].
+			# Stemming the words 
+			stemmed = [stemmer.stem(t) for t in texts]
+			context +=  stemmed 
+		out_str=' '.join(context)
 		doc_context.append(out_str)
-		doc_label += [ 1 ] if file[0] == 'p' else [ 0 ]
-		print out_str
+		doc_label += [ 'pos' ] if file[0] == 'p' else [ 'neg' ]
+		# print out_str
 
+dict = {'Comments': doc_context, 'Category': doc_label}
 
-
+df = pd.DataFrame(dict) 
+df.to_csv('comments.csv', header=True, index=False )
